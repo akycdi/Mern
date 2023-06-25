d1dca83eddcedcf6fa41b1f69eb60cb4a83b3cdf
Arun <arun36824@gmail.com>
2023-06-25 20:55:20 +0530

    WIP react in vanilla

--- Own/Week3/Todo/views/js/index.js
+++ Own/Week3/Todo/views/js/index.js
@@ -1,3 +1,49 @@
+function todosCallback(data) {
+    var parentElement = document.getElementById("parent");
+
+    for (var i = 0; i < data.length; i++) {
+        var childElement = document.createElement("div");
+
+        var grandChildElement1 = document.createElement("span");
+        grandChildElement1.innerHTML = data[i].title
+
+        var grandChildElement2 = document.createElement("span");
+        grandChildElement2.innerHTML = data[i].description
+
+        var grandChildElement3 = document.createElement("button");
+        grandChildElement3.innerHTML = "Delete"
+
+        grandChildElement3.setAttribute("onclick", "deleteTodo(" + data[i].id + " )");
+        childElement.appendChild(grandChildElement1)
+        childElement.appendChild(grandChildElement2)
+        childElement.appendChild(grandChildElement3)
+
+        parentElement.appendChild(childElement);
+    }
+}
+
+function deleteDone(){
+    console.log("delete done");
+}
+
+
+function deleteTodo(id) {
+    fetch("http://localhost:3000/todos/" + id, {
+        method: "DELETE"
+    }).then(deleteDone)
+}
+
+function getDataCallback(resp) {
+    resp.json().then(todosCallback);
+
+}
+
+function getData() {
+    fetch("http://localhost:3000/todos", {
+        method: "GET",
+    }).then(getDataCallback)
+}
+
 function parsedResponse(data) {
     console.log(data);
     getData();
@@ -7,33 +53,18 @@ function callback(resp) {
     resp.json().then(parsedResponse);
 }
 
-async function onPress() {
-    var title = await document.getElementById("title").value;
-    var description = await document.getElementById("description").value;
+function onPress() {
+    var title = document.getElementById("title").value;
+    var description = document.getElementById("description").value;
+
     fetch("http://localhost:3000/todos", {
         method: "POST",
-        body: JSON.stringify(
-            { title: title, description: description }
-        ),
+        body: JSON.stringify({
+            title: title,
+            description: description
+        }),
         headers: {
             "Content-Type": "application/json"
         }
     }).then(callback)
-}
-
-
-function todosCallback(data) {
-    console.log(data);
-    var parentElement = document.getElementById("data");
-    parentElement.innerHTML = JSON.stringify(data);
-}
-
-function getDataCallback(resp) {
-    resp.json().then(todosCallback);
-}
-
-function getData() {
-    fetch("http://localhost:3000/todos", {
-        method: "GET",
-    }).then(getDataCallback);
 }

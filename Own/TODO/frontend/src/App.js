94a0290ba7ecbffcb499dafe84ba22e0e6feae9a
Arun <arun36824@gmail.com>
2023-07-13 10:51:41 +0530

    WIP added Home

--- Own/TODO/frontend/src/App.js
+++ Own/TODO/frontend/src/App.js
@@ -3,16 +3,18 @@ import Todo from './components/Todo';
 import Signup from './components/Signup';
 import AppBarRender from './components/AppBar';
 import Login from './components/Login';
+import Home from './components/Home';
 
-import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Router>
-          <AppBarRender />
+        <AppBarRender />
+        <Home />
         <Routes>
-          <Route path="/" element={<Signup />} />
+          {/* <Route path="/" element={<Signup />} /> */}
           <Route path="/signup" element={<Signup />} />
           <Route path="/todo" element={<Todo />} />
           <Route path="/login" element={<Login />} />
